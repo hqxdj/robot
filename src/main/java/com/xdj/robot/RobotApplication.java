4bba8a712f2d636299d64880896189db209681a6
谢道吉 <13338368241@163.com>
2021-04-15 18:15:51 +0800

    11

--- src/main/java/com/xdj/robot/RobotApplication.java
+++ src/main/java/com/xdj/robot/RobotApplication.java
@@ -2,12 +2,13 @@ package com.xdj.robot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RobotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RobotApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(RobotApplication.class, args);
     }
 
 }
