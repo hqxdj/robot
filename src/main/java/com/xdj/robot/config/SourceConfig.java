b291d3b66313325b9e277c98af4b0ec9c7ec4efc
谢道吉 <13338368241@163.com>
2021-04-16 16:18:16 +0800

    添加mybatis-plus

---
+++ src/main/java/com/xdj/robot/config/SourceConfig.java
@@ -0,0 +1,12 @@
+package com.xdj.robot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SourceConfig {
+
+
+
+}
