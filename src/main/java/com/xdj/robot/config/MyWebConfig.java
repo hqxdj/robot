9b93ec3d835e20239d9c9ecfd7e956d6c14f1d63
谢道吉 <13338368241@163.com>
2021-05-06 09:22:46 +0800

    引入第三方依赖，本地依赖测试

--- src/main/java/com/xdj/robot/config/MyWebConfig.java
+++ src/main/java/com/xdj/robot/config/MyWebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class MyWebConfig implements WebMvcConfigurer {
 
 
 }
--- src/main/java/com/xdj/robot/controller/UsersController.java
+++ src/main/java/com/xdj/robot/controller/UsersController.java
@@ -4,7 +4,6 @@ package com.xdj.robot.controller;
 import com.xdj.robot.model.auto.Users;
 import com.xdj.robot.service.IUsersService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
